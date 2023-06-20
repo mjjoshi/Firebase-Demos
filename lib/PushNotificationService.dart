d3bc7717ece08deb154b524e1c47e282261f922c
kishanvajani <kishan.vajani@openxcell.com>
2023-06-20 10:46:56 +0530

    - Redirect to screen if app is killed

--- lib/PushNotificationService.dart
+++ lib/PushNotificationService.dart
@@ -3,9 +3,6 @@ import 'dart:io';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebasedemos/secondscreen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/navigator.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:get/get.dart';
 
